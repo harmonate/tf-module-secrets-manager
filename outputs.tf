69af50e54b5b17fc5a43f13406141d315b229a38
mptsolutioning <167113569+mptsolutioning@users.noreply.github.com>
2024-06-28 13:42:18 -0700

    v1.2

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "secret_id" {
   description = "The ID of the secret"
   value       = aws_secretsmanager_secret.credentials.id
 }
+
+output "secret_version_id" {
+  description = "The secret ID of the secret version"
+  value       = aws_secretsmanager_secret_version.secret_version.secret_id
+}
